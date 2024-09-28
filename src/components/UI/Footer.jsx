7cb2ac1ff436cc57b4bf3efd3a87a1f18c4dfb5f
kenparungao <kenparungao.024@gmail.com>
2024-09-28 12:37:24 +0800

    [UI Changes] cursor

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -20,18 +20,18 @@ const Footer = () => {
     <footer className="rounded-lg shadow dark:bg-black/85 m-4 backdrop-blur-xl">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <a onClick={handleBackToTop} className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover: cursor-pointer">
             <img src={Logo} className="h-8" alt="ByteQuiz Logo" />
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a onClick={handleBackToTop} className="hover:underline me-4 md:me-6">Back to Top</a>
+              <a onClick={handleBackToTop} className="hover:underline cursor-pointer me-4 md:me-6">Back to Top</a>
             </li>
             <li>
-              <a onClick={() => handleScroll('SingleplayerMode')} className="hover:underline me-4 md:me-6">Singleplayer Mode</a>
+              <a onClick={() => handleScroll('SingleplayerMode')} className="hover:underline cursor-pointer me-4 md:me-6">Singleplayer Mode</a>
             </li>
             <li>
-              <a onClick={() => handleScroll('MultiplayerMode')} className="hover:underline me-4 md:me-6">Multiplayer Mode</a>
+              <a onClick={() => handleScroll('MultiplayerMode')} className="hover:underline cursor-pointer me-4 md:me-6">Multiplayer Mode</a>
             </li>
           </ul>
         </div>
