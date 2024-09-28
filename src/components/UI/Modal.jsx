8ab8f737baa7c0870b3f2c082746e65e08ac3760
kenparungao <kenparungao.024@gmail.com>
2024-09-28 22:07:07 +0800

    [UI Changes] Modal Close Button Responsiveness

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Modal = ({ onClose, children }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-hidden bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-hidden bg-black bg-opacity-50 px-4">
       <div className="relative bg-white rounded-lg shadow-lg w-fit overflow-x-auto overflow-y-auto">
         {/* Modal close button */}
-        <div className="flex justify-end p-4">
+        <div className="sticky left-2 justify-end p-4">
           <button
             onClick={onClose}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 me-2 mb-2"
+            className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm pl-6 pr-6 py-3 mb-2 items-center"
           >
             Close
           </button>
