d2d3f147ee0feef0b7108fd73237c0aa4e3ff303
thisellie <81550623+thisellie@users.noreply.github.com>
2024-11-08 10:53:36 +0800

    Revert "Add `1.2.0` apk download link"
    This reverts commit 4b1544d595f179eeceaebb7c91dd42b00c4e80a5.

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -31,11 +31,11 @@ const HeroSection = () => {
                 <img src={Logo} alt="Logo" className="mb-4 mx-auto w-3/4 sm:w-2/3 lg:w-2/3 xl:w-3/4" />
                 <p className="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 xl:px-48">ByteQuiz brings improvements on academic performance by introducing gamification to enhance engagement and supplement your knowledge on educational lessons.</p>
                 <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                    <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-blue-700">
+                    <a href="https://github.com/belsua/ByteQuiz/releases/download/v1.2.0/bytequiz-1.2.0.apk" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-blue-700">
                         Download for Android
                         <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </a>
-                    <a href="https://github.com/belsua/ByteQuiz/releases/download/v1.2.0/bytequiz-1.2.0.apk" className="hidden inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center bg-white/40 backdrop-blur-sm text-white rounded-lg border border-gray-300 hover:bg-white hover:text-blue-700">
+                    <a href="#" className="hidden inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center bg-white/40 backdrop-blur-sm text-white rounded-lg border border-gray-300 hover:bg-white hover:text-blue-700">
                         <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2  1z"></path></svg>
                         Watch video
                     </a>  
