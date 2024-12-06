a59a341789c4b80a7225e19e72ce82be21f9a36d
thisellie <81550623+thisellie@users.noreply.github.com>
2024-12-06 11:55:17 +0800

    Update hero image to `.webp`

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,6 +1,6 @@
 // src/components/HeroSection.jsx
 import React from 'react'
-import Logo from '../../assets/Logo.png'
+import Logo from '../../assets/Logo.webp'
 import backgroundImage from '../../assets/Sample.webp'
 
 const HeroSection = () => {
